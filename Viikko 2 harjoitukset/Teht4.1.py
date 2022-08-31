6139584cfa257e0825f8c8d5f88236136124acf5
samwise50000 <sami.mattila3@metropolia.fi>
2022-08-31 23:06:53 +0300

    Harjoitukset

--- Viikko 2 harjoitukset/Teht4.1.py
+++ Viikko 2 harjoitukset/Teht4.1.py
@@ -1,6 +1,12 @@
-#Kirjoita while-toistorakennetta käyttävä ohjelma,
-#joka tulostaa kolmella jaolliset luvut väliltä 1..1000.
+# Kirjoita while-toistorakennetta käyttävä ohjelma,
+# joka tulostaa kolmella jaolliset luvut väliltä 1..1000.
 
 # Write a program that uses the while-loop,
 # that prints numbers only dividable by 3 between numbers 1....1000
 
+number = 1
+
+while number <= 1000:
+    number += 1
+    if number % 3 == 0:
+        print(number)
