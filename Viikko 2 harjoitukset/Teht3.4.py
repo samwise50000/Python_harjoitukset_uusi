8aa874df4ca293dfab873aec9d0e50ee00b24cab
samwise50000 <sami.mattila3@metropolia.fi>
2022-08-31 22:43:11 +0300

    Harjoitukset

---
+++ Viikko 2 harjoitukset/Teht3.4.py
@@ -0,0 +1,16 @@
+# Kirjoita ohjelma, joka kysyy vuosiluvun ja ilmoittaa, onko annettu vuosi karkausvuosi.
+# Vuosi on karkausvuosi, jos se on jaollinen neljällä.
+# Sadalla jaolliset vuodet ovat karkausvuosia vain jos ne ovat jaollisia myös neljälläsadalla.
+
+year = int(input('Type a year: '))
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print('Leap year.')
+        else:
+            print('Not a leap year.')
+    else:
+        print('Leap year.')
+else:
+    print('Not a leap year.')
