026dcf049c28ea9a19eb599a760f25be878695c9
samwise50000 <sami.mattila3@metropolia.fi>
2022-09-02 09:23:54 +0300

    Harjoitukset

--- Viikko 2 harjoitukset/Teht4.5.py
+++ Viikko 2 harjoitukset/Teht4.5.py
@@ -4,12 +4,6 @@
 # Edellisessä tapauksessa tulostetaan Tervetuloa ja jälkimmäisessä Pääsy evätty.
 # (Oikea käyttäjätunnus on python ja salasana rules).
 
-# Write a program, that asks the user an username and a password.
-# If either one or both are wrong, the username and password will be asked again.
-# This will be continued until the login information are correct OR if the login info has been input wrong 5 times.
-# In previous case the print will be: "Welcome" and the latter: "Access denied."
-# (The username is "python" and the password "rules").
-
 username = "python"
 password = "rules"
 loginSucceeded = False
