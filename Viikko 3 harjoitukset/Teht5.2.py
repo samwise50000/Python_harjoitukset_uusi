b29ec33bf791b0b81da414b275a4e05c60900cf9
samwise50000 <sami.mattila3@metropolia.fi>
2022-09-05 14:57:26 +0300

    Harjoitukset

--- Viikko 3 harjoitukset/Teht5.2.py
+++ Viikko 3 harjoitukset/Teht5.2.py
@@ -1,6 +1,9 @@
 # Kirjoita ohjelma, joka kysyy käyttäjältä lukuja siihen saakka, kunnes tämä syöttää tyhjän merkkijonon lopetusmerkiksi.
 # Lopuksi ohjelma tulostaa saaduista luvuista viisi suurinta suuruusjärjestyksessä suurimmasta alkaen.
 # Vihje: listan alkioiden lajittelujärjestyksen voi kääntää antamalla sort-metodille argumentiksi reverse=True.
+#for number in numbers[:5]:
+    #print(number)
+
 
 numbers = []
 readingNumbers = True
