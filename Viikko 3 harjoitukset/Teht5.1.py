5018c6132f529b883e1db37d03292731ea518537
samwise50000 <sami.mattila3@metropolia.fi>
2022-09-05 11:24:26 +0300

    Harjoitukset

--- Viikko 3 harjoitukset/Teht5.1.py
+++ Viikko 3 harjoitukset/Teht5.1.py
@@ -1,3 +1,12 @@
-#test
+#Kirjoita ohjelma, joka kysyy käyttäjältä arpakuutioiden lukumäärän.
+#Ohjelma heittää kerran kaikkia arpakuutioita ja tulostaa silmälukujen summan. Käytä for-toistorakennetta.
+from random import randint
 
-i = 0
+nop_yht = []
+
+nopat = int(input("Montako noppaa heitetään?: "))
+
+for nopat in range(nopat):
+
+
+print("Noppien summa on:", )
