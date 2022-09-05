812befb2a8c048ed4d95cd9b4c416ecbd031d1ba
samwise50000 <sami.mattila3@metropolia.fi>
2022-09-05 10:47:59 +0300

    Harjoitukset

--- Viikko 2 harjoitukset/Teht4.6.py
+++ Viikko 2 harjoitukset/Teht4.6.py
@@ -23,9 +23,9 @@ i = 0
 n = int(input("Anna arvottavien pisteiden määrä: "))
 
 while i <= n:
-    x = random.randint(-1, 1)
-    y = random.randint(-1, 1)
-    if x ^ 2 + y ^ 2 <= 1:
+    x = random.uniform(-1, 1)
+    y = random.uniform(-1, 1)
+    if x ** 2 + y ** 2 <= 1:
         sisalla += 1
     i += 1
 
