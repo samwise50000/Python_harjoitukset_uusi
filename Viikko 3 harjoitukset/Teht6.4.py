86ac13b8a9a68c56222f10176af979b3355dcb00
samwise50000 <sami.mattila3@metropolia.fi>
2022-09-07 21:29:25 +0300

    Harjoitukset

---
+++ Viikko 3 harjoitukset/Teht6.4.py
@@ -0,0 +1,24 @@
+# Kirjoita funktio, joka saa parametrinaan listan kokonaislukuja.
+# Ohjelma palauttaa listassa olevien lukujen summan.
+# Kirjoita testausta varten pääohjelma, jossa luot listan, kutsut funktiota ja tulostat sen palauttaman summan.
+
+# Write a function, that gets a parameter of a list of whole numbers.
+# The program returns the sum of whole numbers in the llist.
+# Write a main program for testing, where you create a list, and call the function to print out the sum inside the function.
+
+
+
+def sumOfNumbers(listOfNumbers):
+    sum = 0
+    for number in listOfNumbers:
+        sum += number
+    return sum
+
+
+listOfNumbers = [0, 5, 3, 2, 7]
+
+sum = sumOfNumbers(listOfNumbers)
+
+print(f"Summa on: {sum}")
+
+
---
+++ Viikko 3 harjoitukset/Teht6.5.py
@@ -0,0 +1,21 @@
+# Kirjoita funktio, joka saa parametrinaan listan kokonaislukuja.
+# Ohjelma palauttaa toisen listan, joka on muuten samanlainen kuin parametrina saatu lista paitsi että siitä on karsittu pois kaikki parittomat luvut.
+# Kirjoita testausta varten pääohjelma, jossa luot listan, kutsut funktiota ja tulostat sen jälkeen sekä alkuperäisen että karsitun listan.
+
+# Write a function, that gets a list of whole numbers into the parameter.
+# The program will return another list, that is same as in the parameter BUT it has every odd number removed.
+# Write a main program, where you create a list. Call out the function and then print out in the main program the first original list and then the list that has odd numbers removed.
+
+def removeOddNumbers(listOfNumbers):
+    for number in listOfNumbers:
+        if number%2 != 0:
+            listOfNumbers.remove(number)
+    return listOfNumbers
+
+
+listOfNumbers = [0, 3, 4, 6, 7]
+
+print(listOfNumbers)
+listOfEvenNumbers = removeOddNumbers(listOfNumbers)
+
+print(listOfEvenNumbers)
