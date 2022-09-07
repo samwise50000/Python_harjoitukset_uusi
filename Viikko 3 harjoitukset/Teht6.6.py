3fb6cf321e584d601a301dff91bd8e0654eedb30
samwise50000 <sami.mattila3@metropolia.fi>
2022-09-07 22:22:16 +0300

    Harjoitukset

---
+++ Viikko 3 harjoitukset/Teht6.6.py
@@ -0,0 +1,44 @@
+# Kirjoita funktio, joka saa parametreinaan pyöreän pizzan halkaisijan senttimetreinä sekä pizzan hinnan euroina.
+# Funktio laskee ja palauttaa pizzan yksikköhinnan euroina per neliömetri.
+# Pääohjelma kysyy käyttäjältä kahden pizzan halkaisijat ja hinnat sekä ilmoittaa,
+# kumpi pizza antaa paremman vastineen rahalle (eli kummalla on alhaisempi yksikköhinta).
+# Yksikköhintojen laskennassa on hyödynnettävä kirjoitettua funktiota.
+
+
+# Write a function, that gets a parameter of the pizza diameter and the value of pizza in euros.
+# The function calculates and returns the value of pizza by euros per square meter.
+# The main program asks the user two pizzas and the diameters of the pizzas and the prices. The program then informs,
+# which one of the two pizzas you will get the better value for (basically which pizza has the cheaper value in total).
+# The price unit calculation has to use a written function
+
+import math
+
+
+def calculatePizzaValuePerSquareMeter1(diameterInCm, priceInEuros):
+    pizzaSurfaceArea = math.pi*diameterInCm
+    priceInEuroPerSquareMetre = pizzaPriceInEuros1 / pizzaSurfaceArea
+    return priceInEuroPerSquareMetre
+
+def calculatePizzaValuePerSquareMeter2(diameterInCm, priceInEuros):
+    pizzaSurfaceArea = math.pi*diameterInCm
+    priceInEuroPerSquareMetre = pizzaPriceInEuros2 / pizzaSurfaceArea
+    return priceInEuroPerSquareMetre
+
+def calculateBestValueForMoneyPizza(pizza1PricePerSquareMetre, pizza2PricePerSquareMetre):
+    if pizza1PricePerSquareMetre < pizza2PricePerSquareMetre:
+        return print(f"Pizza 1 on halvempi, pizzan hinta on {pizza1PricePerSquareMetre:.2f} per neliömetri.")
+    else:
+        return print(f"Pizza 2 on halvempi, pizzan hinta on {pizza2PricePerSquareMetre:.2f} per neliömetri.")
+
+pizzaDiameterInCm1 = float(input('Input the diameter for pizza 1 in cm: '))
+pizzaPriceInEuros1 = float(input('Input the price for pizza 1 in euros: '))
+
+pizzaDiameterInCm2 = float(input('Input the diameter for pizza 2 in cm: '))
+pizzaPriceInEuros2 = float(input('Input the price for pizza 2 in euros: '))
+
+pizza1PricePerSquareMetre = calculatePizzaValuePerSquareMeter1(pizzaDiameterInCm1, pizzaPriceInEuros1)
+pizza2PricePerSquareMetre = calculatePizzaValuePerSquareMeter2(pizzaDiameterInCm2, pizzaPriceInEuros2)
+
+bestValueForMoneyPizza = calculateBestValueForMoneyPizza(pizza1PricePerSquareMetre, pizza2PricePerSquareMetre)
+#print(bestValueForMoneyPizza)
+
