998848163d5632bcdeb6659e685f1aecef9d4912
samwise50000 <sami.mattila3@metropolia.fi>
2022-09-12 21:05:21 +0300

    Harjoitukset

--- Viikko_4_harjoitukset/Teht8.2.py
+++ Viikko_4_harjoitukset/Teht8.2.py
@@ -1,5 +1,7 @@
-# Kirjoita ohjelma, joka kysyy käyttäjältä maakoodin (esimerkiksi FI) ja tulostaa kyseisessä maassa olevien lentokenttien lukumäärät tyypeittäin.
-# Esimerkiksi Suomen osalta tuloksena on saatava tieto siitä, että pieniä lentokenttiä on 65 kappaletta, helikopterikenttiä on 15 kappaletta jne.
+# Kirjoita ohjelma, joka kysyy käyttäjältä maakoodin (esimerkiksi FI) ja
+# tulostaa kyseisessä maassa olevien lentokenttien lukumäärät tyypeittäin.
+# Esimerkiksi Suomen osalta tuloksena on saatava tieto siitä,
+# että pieniä lentokenttiä on 65 kappaletta, helikopterikenttiä on 15 kappaletta jne.
 
 import mysql.connector
 "SELECT type, COUNT*) AS FROM airport WHERE iso_country * 'SE' GROUP BY TYPE"
@@ -12,7 +14,7 @@ yhteys = mysql.connector.connect(
          password='SeOnSiina!?',
          autocommit=True
          )
-maakoodi = input("Anna 2 kirjaiminen maakoodi")
+maakoodi = input("Anna 2 kirjaiminen maakoodi: ")
 sql = "SELECT name, continent FROM country WHERE iso_country = '" + maakoodi +"'"
 print(sql)
 
