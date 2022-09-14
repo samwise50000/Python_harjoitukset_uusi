bd677f81f685cac68e1b90c39ab7cc2ccc8bfc4a
samwise50000 <sami.mattila3@metropolia.fi>
2022-09-14 13:07:19 +0300

    Harjoitukset

--- Viikko_4_harjoitukset/Teht8.3.py
+++ Viikko_4_harjoitukset/Teht8.3.py
@@ -4,8 +4,8 @@
 # Laske etäisyys geopy-kirjaston avulla: https://geopy.readthedocs.io/en/stable/. Asenna kirjasto valitsemalla View / Tool Windows / Python Packages.
 # Kirjoita hakukenttään geopy ja vie asennus loppuun.
 
+from geopy.distance import geodesic
 import mysql.connector
-
 yhteys = mysql.connector.connect(
          host='127.0.0.1',
          port= 3306,
@@ -14,3 +14,18 @@ yhteys = mysql.connector.connect(
          password='SeOnSiina!?',
          autocommit=True
          )
+
+def hae(lentoasema):
+    sql = "select latitude_deg, longtitude_deg from airport where ident = '\"" + lentoasema + "\"';"
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
+    return tulos
+
+x=input("Anna ensimmäinen ICAO-koodi: ")
+x=x.upper()
+hae(x)
+y=input("Anna toinen ICAO-koodi: ")
+y=y.upper()
+hae(y)
+print(f"{geodesic(hae(x),hae(y)).km:0.2f} kilometriä välimatkaa")
